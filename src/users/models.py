007f8247fee3f8cd14f6b783e722bd9ffe03fd0d
bradyjibanez <bradyjibanez@gmail.com>
2018-11-29 22:10:29 -0500

    Cleaner

--- src/users/models.py
+++ src/users/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class User(models.Model):
 	name 			= models.CharField(max_length = 100)
 	password	 	= models.TextField(default = "")
+
+class Script(models.Model):
+	name			= models.CharField(max_length = 100)
+
+class Command(models.Model):
+	name			= models.CharField(max_length = 100)
