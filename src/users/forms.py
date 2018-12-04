815b729d8ba39aa1ef2f1422e2d455ff24dce564
TSomann <taylorsomann@gmail.com>
2018-12-04 17:39:52 -0500

    Please

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,17 +1,5 @@
 from django import forms
-from djangocodemirror.frields import CodeMirrorField
+from djangocodemirror.fields import CodeMirrorField
 #from codemirror import CodeMirrorTextarea
 #from codemirror.widgets import CodeMirrorTextarea
 
-codemirror_window = CodeMirrorTextarea(
-	mode="python",
-	theme="cobalt",
-	config={
-		'fixedGutter': True
-	},
-)
-
-document = forms.CharField(widget=codemirror_window)
-
-class SampleForm(forms.Form):
-	test = CodeMirrorField(label="Test", required=True, config_name="restructredtext")
